59c644ecb2f5ea5c0cbcf65dcf97cb67917287e7
cnt7-naya-cdh6 <naya@cnt7-naya-cdh6.org>
2019-12-09 21:10:36 +0200

    mysql consumer

--- configure_environment.sh
+++ configure_environment.sh
@@ -1,8 +1,7 @@
 # Install nbdime, a plugin for Jupyter that visualises difference in notebooks
 
 pip install --upgrade nbdime
-#conda install nodejs
-#conda install npm
+conda install nodejs
+conda install npm
 nbdime extensions --enable
 
-#hello 2
