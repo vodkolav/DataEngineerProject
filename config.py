b8d97665bddd28f533a5caa797a314a31e8c7487
cnt7-naya-cdh6 <naya@cnt7-naya-cdh6.org>
2020-01-18 15:25:33 +0200

    Some cleaning up  -all variables in config file sorted out  -code samples for threading in Sandbox
    Signed-off-by: cnt7-naya-cdh6 <naya@cnt7-naya-cdh6.org>

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 from pyspark.sql.types import StructType, StringType, MapType, StructField,\
-                              BooleanType, DateType, NumericType, IntegerType,\
-                              LongType, TimestampType, FloatType, ArrayType
+                             BooleanType, DateType, NumericType, IntegerType,\
+                             LongType, TimestampType, FloatType, ArrayType
 import pyarrow as pa
-
 
 topic = 'TweeterArchive'
 partitionCol = "created_ym"
@@ -10,7 +9,7 @@ Keywords = 'Tesla'
 
 
 hdfs_host = 'localhost'
-hdfs_port = 9870
+hdfs_port = 8020 #9870
 hive_port = 10000
 hive_username = 'hdfs'
 hive_password = 'naya'
@@ -18,12 +17,10 @@ hive_database = 'tesla'
 hive_mode = 'CUSTOM'
 
 hdfs_archive_path = '/tmp/project/archive'    
-hdfs_archive_checkpoint_path = hdfs_archive_path + '/sparkcheckpoint'
+hdfs_archive_path
 
-hdfs_hive_warehouse = '/user/hive/warehouse/'    
-
-hdfs_hive_staging = '/tmp/project/hive/tweets'
-hdfs_hive_staging_checkpoint = hdfs_hive_staging + '/sparkcheckpoint'
+hdfs_hive_warehouse = '/user/hive/warehouse/' 
+hdfs_hive_warehouse
 
 hdfs_hive_tweets = f"{hdfs_hive_warehouse}/{hive_database}.db/tweets"
 hdfs_hive_tweets
@@ -34,21 +31,11 @@ hdfs_hive_users
 hdfs_hive_users_staging = f"{hdfs_hive_warehouse}/{hive_database}.db/users_staging"
 hdfs_hive_users_staging
 
-
-fs = pa.hdfs.connect(
-    host=hdfs_host, 
-    port=8020, 
-    user=hive_username, 
-    kerb_ticket=None, 
-    driver='libhdfs', 
-    extra_conf=None)
-
-
-
-
+#used in tweeter_producer
 event_fields = [ 'id', 'text','created_at', 'geo', 'coordinates', 'place',
                  'quote_count', 'reply_count', 'retweet_count', 'favorite_count' ]
 
+#used in Spark_consumer
 tweet_keys =  event_fields + ['user_id', 'user_followers' ]
 
 tweet_types = [LongType, StringType, TimestampType, StringType, StringType, StringType, 
@@ -56,11 +43,11 @@ tweet_types = [LongType, StringType, TimestampType, StringType, StringType, Stri
 
 # tweet_types  = [StringType]* 11 
 
-
+#used in tweeter_producer
 user_fields = ['id', 'name', 'screen_name','created_at', 'location', 'url',
                          'protected', 'verified', 'followers_count', 'friends_count',
                          'listed_count', 'favourites_count', 'statuses_count', 'withheld_in_countries']
-
+#used in Spark_consumer
 user_keys = user_fields + [partitionCol]
 
 user_types = [LongType, StringType, StringType, TimestampType, StringType, StringType, 
